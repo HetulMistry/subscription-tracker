f352424379f68d2e1fb575af9f36d9ef3ca0ebf1
Hetul Mistry <85955968+HetulMistry@users.noreply.github.com>
2026-01-08 20:05:15 +0530

    feat: configure MongoDB connection and update environment variables

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import { PORT } from "./config/env.js";
 import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
+import connectDb from "./database/mongodb.js";
 
 const app = express();
 
@@ -15,8 +16,10 @@ app.get("/", (req, res) => {
     res.send("Hello, World!");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
     console.log(`Server is running on http://localhost:${PORT}`);
+
+    await connectDb();
 });
 
 export default app;
